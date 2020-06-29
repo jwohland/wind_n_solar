71601d82e3ef0f65f478c5b9230eb62597573ae5
jwohland <jwohland@ethz.ch>
2020-06-29 16:43:23 +0200

    Initial commit

---
+++ wind/trend_correction/ensemble_spread.py
@@ -0,0 +1,75 @@
+import xarray as xr
+import matplotlib.pyplot as plt
+import glob
+import matplotlib as mpl
+
+mpl.use('Agg')
+
+
+def identify_representative(ds):
+    """
+    Identification of most representative ensemble member
+    Based on a comparison of spatio-temporal averages, one of the ensemble members next to the median are chosen
+    :param ds: input dataset
+    :return:
+    """
+    tmp = ds['s100_low'].mean(dim=['time', 'longitude', 'latitude']).load()
+    pseudo_median = sorted(tmp.values)[int(tmp.size / 2)]  # not strictly the median because a median is not contained
+    # in the input data if the sample size is even
+    return tmp.where(tmp == pseudo_median, drop=True).number.values[0]
+
+
+data_path = '/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/output/'
+plot_path = '/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/plots/ensemble_spread/'
+colors = ['peru', 'olive']
+
+# Identification of representative ensemble members
+noaa_data = xr.open_mfdataset(glob.glob(data_path + '20CR*_latindex_[15 25 35]*.nc'), combine='by_coords')
+cera_data = xr.open_mfdataset(glob.glob(data_path + 'CERA*_latindex_[15 25 35]*.nc'), combine='by_coords')
+rep_noaa, rep_cera = identify_representative(noaa_data), identify_representative(cera_data)
+
+for lat in [15, 25, 35]:
+    noaa_data = xr.open_mfdataset(glob.glob(data_path + '20CR*_latindex_' + str(lat) + '*.nc'), combine='by_coords')
+    cera_data = xr.open_mfdataset(glob.glob(data_path + 'CERA*_latindex_' + str(lat) + '*.nc'), combine='by_coords')
+    for lon in noaa_data.longitude.values:
+        # plot absolute timeseries
+        print(lon)
+        f, ax = plt.subplots()
+        for i, data in enumerate([noaa_data, cera_data]):
+            name = '20CRv3 ' if i == 0 else 'CERA20C '
+            tmp_data = data.sel({'longitude': lon, 'latitude': data.latitude.values[0]})
+            for number in data.number.values:
+                # grey color for representative member
+                color = 'black' if ((i == 0 and number == rep_noaa) or (i == 1 and number == rep_cera)) else colors[i]
+                tmp_data['s100_low'].sel({'number': number}).plot(ax=ax,
+                                                                  label=name if number == 1 else '',
+                                                                  color=color,
+                                                                  alpha=.8)
+        ax.set_title('Lat =' + str(data.latitude.values[0]) + ', Lon = ' + str(lon))
+        ax.legend(loc=1)
+        plt.tight_layout()
+        ax.set_ylabel('Lanczos-filtered 100m wind speed [m/s]')
+        plt.savefig(plot_path + str(lat) + '/Abs_lat_' + str(data.latitude.values[0]) + '_lon_' + str(lon) + '.jpeg',
+                    dpi=300)
+        plt.close('all')
+
+    for lon in noaa_data.longitude.values:
+        # plot differences
+        f, ax = plt.subplots()
+        tmp_noaa, tmp_cera = noaa_data.sel({'longitude': lon, 'latitude': noaa_data.latitude.values[0]}), \
+                             cera_data.sel({'longitude': lon, 'latitude': cera_data.latitude.values[0]})
+        for noaa_num in tmp_noaa.number.values:
+            for cera_num in tmp_cera.number.values:
+                diff = tmp_noaa['s100_low'].sel({'number': noaa_num}) - tmp_cera['s100_low'].sel({'number': cera_num})
+                if ((noaa_num == rep_noaa) and (cera_num == rep_cera)):
+                    diff.plot(ax=ax, color='black', alpha=.8, lw=3)
+                else:
+                    diff.plot(ax=ax, color='indigo', alpha=.2, lw=1)
+        ax.set_title('Lat =' + str(noaa_data.latitude.values[0]) + ', Lon = ' + str(lon))
+        ax.set_ylabel('Difference in filtered 100m wind speed [m/s]')
+        ax.set_ylim(ymin=-1, ymax=3)
+        ax.axhline(y=0, ls='--', color='grey')
+        plt.tight_layout()
+        plt.savefig(
+            plot_path + str(lat) + '/Diff_lat_' + str(noaa_data.latitude.values[0]) + '_lon_' + str(lon) + '.jpeg')
+        plt.close('all')
