d3197887c924e59348043592a686d3193038c3c8
jwohland <jwohland@ethz.ch>
2020-06-30 14:35:19 +0200

    updated paths and implemented trend correction

--- wind/trend_correction/compute_corrected.py
+++ wind/trend_correction/compute_corrected.py
@@ -10,10 +10,10 @@ The approach consists of
 
 This script does d).
 """
-from itertools import permutations
 import glob
-import sys
 import xarray as xr
+import pandas as pd
+import numpy as np
 
 
 def build_filedic(data_path, lanczos_path):
@@ -23,67 +23,51 @@ def build_filedic(data_path, lanczos_path):
     :param lanczos_path: path to lanczos filtered data
     :return:
     """
-    filedic = {}
-    filedic['CERA'] = glob.glob(data_path + 'CERA20C/' + '*instant.nc')
-    filedic['lanczos(CERA)'] = glob.glob(lanczos_path + '*CERA20C*.nc')
-    filedic['lanczos(20CR)'] = glob.glob(lanczos_path + '*20CR*.nc')
+    filedic = {'CERA': sorted(glob.glob(data_path + 'CERA20C/*.nc')),
+               'lanczos(CERA)': sorted(glob.glob(lanczos_path + 'CERA_7*.nc')),
+               'lanczos(20CR)': sorted(glob.glob(lanczos_path + '20CRv3_5*.nc'))}
     return filedic
 
 
-def determine_combination(file_dic, file_index):
+def drop_non_wind(ds):
     """
-    Maps scalar file_index to a combination of a CERA and 20CR ensemble member
-    :param file_index:
+    Preprocessing function to drop all variables that are not wind.
+    :param ds: CERA20C dataset
     :return:
     """
-    perms = list(permutations(range(len(file_dic['lanczos(CERA'])), 2))
-    if file_index > len(perms):
-        raise ValueError('file_index larger than number of permutations')
-    cera_index, noaa_index = perms[file_index]
-    lanczos_cera_path = file_dic['lanczos(CERA)'][cera_index]
-    cera_stream = lanczos_cera_path.split('_')[1]  # todo this assumes that the stream number is given, not the index
-    lanczos_20cr_path = file_dic['lanczos(20CR)'][noaa_index]
-    return cera_stream, lanczos_cera_path, lanczos_20cr_path
+    return ds.drop(['fdir', 't2m', 'ssrd'])
 
 
-def correct_longterm(s_raw, s_lanczos_cera, s_lanczos_noaa):
+def calculate_correction(filedic):
     """
-    :param s_raw: 3h raw CERA20C wind speed data
-    :param s_lanczos_cera: lanczos filtered CERA20C wind speed data
-    :param s_lanczos_noaa: lanczos filtered 20CR wind speed data
+    calculates low-frequency correction term
+    :param filedic: dictionary including lanczos filtered versions of CERA20C and 20CRv3
     :return:
     """
-    return s_raw - s_lanczos_cera + s_lanczos_noaa
+    lanczos_cera = xr.open_mfdataset(filedic['lanczos(CERA)'], combine='by_coords')
+    lanczos_noaa = xr.open_mfdataset(filedic['lanczos(20CR)'], combine='by_coords')
+    return lanczos_noaa.drop('number').squeeze() - lanczos_cera.drop('number').squeeze()
 
 
-def open_files(cera_stream, filedic, lanczos_cera_path, lanczos_noaa_path):
-    s_raw = xr.open_mfdataset(filedic['CERA'],
-                              chunks={'latitude': 5, 'longitude': 5},
-                              combine='by_coords')
-    s_raw.sel({'number': cera_stream}, drop=True)
-    s_lanczos_cera = xr.open_mfdataset(lanczos_cera_path,
-                                       chunks={'latitude': 5, 'longitude': 5},
-                                       combine='by_coords')
-    s_lanczos_noaa = xr.open_mfdataset(lanczos_noaa_path,
-                                       chunks={'latitude': 5, 'longitude': 5},
-                                       combine='by_coords')
-    return s_raw, s_lanczos_cera, s_lanczos_noaa
-
-
-def main(file_index):
-    data_path = '/cluster/work/apatt/wojan/data/'  # todo update path
-    lanczos_path = '/cluster/work/apatt/wojan/data/'  # todo update path
-    out_path = ''  # todo update path
+def main():
+    data_path = '/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/data/'
+    lanczos_path = '/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/output/lanczos/'
+    out_path = '/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/output/trend_corrected/'
 
     filedic = build_filedic(data_path, lanczos_path)
-    cera_stream, lanczos_cera_path, lanczos_noaa_path = determine_combination(filedic, file_index)
-    s_raw, s_lanczos_cera, s_lanczos_noaa = open_files(cera_stream, filedic, lanczos_cera_path, lanczos_noaa_path)
-    s_corrected = correct_longterm(s_raw, s_lanczos_cera, s_lanczos_noaa)
-    noaa_member = '3'  # todo infer member number
+    diff = calculate_correction(filedic)
+    diff.load()
+    # calculate for individual years for memory reasons
+    for year_index in range(len(filedic['CERA'])):
+        cera_raw = xr.open_mfdataset(filedic['CERA'][year_index], combine='by_coords', preprocess=drop_non_wind)
+        cera_corrected = (cera_raw['s100'] - diff['s100_low']).to_dataset(name='s100')
+        cera_corrected.compute()
+        year = pd.to_datetime(cera_corrected.time[0].values).year
+        print(year)
+        name = 'CERA20C_correcteds100_' + str(year) + '_representative.nc'
+        # some years consist of nans only (because of filter boundary effects)
+        if not np.isnan(cera_corrected['s100'].values).all():
+            cera_corrected.to_netcdf(out_path + name)
 
-    name = 'corrected_s100_cera_' + cera_stream + '_noaa_' + noaa_member + '.nc'
-    s_corrected.to_netcdf(out_path + name)
 
-
-file_index = int(sys.argv[1])
-main(file_index)
+main()
