dac79f83d09c1913106dd79a1d701639c14a39da
jwohland <jwohland@ethz.ch>
2020-07-08 13:45:15 +0200

    changed to a job array
    Memory reduced to 7000 MB as 1914 test run needed 5.5 MB max

--- wind/wind_power_conversion/submit_windpower.sh
+++ wind/wind_power_conversion/submit_windpower.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-#BSUB -J calculate_windpower_test
+#BSUB -J calculate_windpower_[1-101]
 #BSUB -n 1
-#BSUB -R "rusage[mem=10000]"
+#BSUB -R "rusage[mem=7000]"
 #BSUB -W 24:00
 #BSUB -r
-#BSUB -o ../../../logs/calculate_windpower_test.txt
+#BSUB -o ../../../logs/calculate_windpower_%I.txt
 
-python windpower.py 10
+python windpower.py $LSB_JOBINDEX
