ce4a2e53063571def11e930cf710b90597979857
jwohland <jwohland@ethz.ch>
2020-07-10 15:37:28 +0200

    only append to log file

--- wind/wind_power_conversion/submit_windpower.sh
+++ wind/wind_power_conversion/submit_windpower.sh
@@ -4,6 +4,6 @@
 #BSUB -R "rusage[mem=7000]"
 #BSUB -W 24:00
 #BSUB -r
-#BSUB -oo ../../../logs/calculate_windpower_%I.txt
+#BSUB -o ../../../logs/calculate_windpower_%I.txt
 
 python windpower.py $LSB_JOBINDEX
