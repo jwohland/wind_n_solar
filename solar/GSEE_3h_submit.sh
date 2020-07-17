9a399954f9d894aeb595d4202e297fd78abe763e
jwohland <jwohland@ethz.ch>
2020-07-17 15:28:37 +0200

    Initial commit

---
+++ solar/GSEE_3h_submit.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#BSUB -J GSEE_3h[1-109]
+#BSUB -R "rusage[mem=8000]"
+#BSUB -n 110
+#BSUB -W 24:00
+#BSUB -r
+#BSUB -oo ../../logs/GSEE_3h_%I.txt
+
+for ensemble_member in {1..10}
+do
+  echo ${ensemble_member}
+  python GSEE_3h_playground.py  ${LSB_JOBINDEX} ${ensemble_member}
+done
