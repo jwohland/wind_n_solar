c7e32c6636d633b10f08a42f479f40e88b1191bb
jwohland <jwohland@ethz.ch>
2020-08-10 10:45:45 +0200

    largely reverted back as jobs were mainly idle if multiple jobs per year were submitted

--- solar/solar_power_conversion/GSEE_3h_submit.sh
+++ solar/solar_power_conversion/GSEE_3h_submit.sh
@@ -1,23 +1,17 @@
 #!/bin/sh
-#BSUB -J GSEE_3h[1-3270]
+#BSUB -J GSEE_3h[1-109]
 #BSUB -R "rusage[mem=2000]"
-#BSUB -W 04:00
+#BSUB -W 72:00
 #BSUB -r
 #BSUB -oo ../../../logs/GSEE_3h_%I.txt
 
-i=1
-for year in {1..109}
+for ensemble_member in {1..10}
 do
-  for ensemble_member in {1..10}
+  echo ${ensemble_member}
+  for scenario in {1..3}
   do
-    for scenario in {1..3}
-    do
-      #echo ${scenario}
-      if [[ i -eq ${LSB_JOBINDEX} ]] ; then
-        echo ${year} ${ensemble_member} ${scenario}
-        python GSEE_3h.py ${year} ${ensemble_member} ${scenario}
-      fi
-      i=$(( $i + 1))
-    done
+    echo ${scenario}
+    python GSEE_3h.py  ${LSB_JOBINDEX} ${ensemble_member} ${scenario}
   done
 done
+
