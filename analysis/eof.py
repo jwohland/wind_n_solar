c85da0e7be501327f3fc70fe8cb3a9cd131ae193
jwohland <jwohland@ethz.ch>
2020-08-17 16:32:37 +0200

    Alow to change plot_path after initialiation of class instance

--- analysis/eof.py
+++ analysis/eof.py
@@ -5,9 +5,10 @@ import sys
 import cartopy.crs as ccrs
 import numpy as np
 import pandas as pd
+import xarray as xr
 
 sys.path.append("../")
-from utils import plot_field, Generation_type, add_letters
+from utils import plot_field, Generation_type, add_letters, add_row_label
 
 mpl.rcParams["axes.spines.left"] = True
 mpl.rcParams["axes.spines.bottom"] = True
@@ -41,14 +42,46 @@ def perform_EOF(power, N):
     return eofs, pcs, variance_fraction
 
 
+def perform_EOF_multi(data, N, coords, datanames):
+    """
+    Perform multivariate EOF analysis of input data Arrays
+    :param data: list of data arrays (first dimension must be time)
+    :param N: number of EOFs to keep
+    :param coords: Grid for eof output (multivariate not defined as xarray operation)
+    :param datanames: Names of input data
+    :return:
+    """
+    from eofs.multivariate.standard import MultivariateEof
+
+    solver = MultivariateEof(data)
+    eofs = np.asarray(solver.eofs(neofs=N))
+    pcs = solver.pcs(npcs=N)
+    variance_fraction = solver.varianceFraction()
+
+    # transform eofs into a dataset
+    tmp = []
+    for i, dataname in enumerate(datanames):
+        tmp2 = []
+        for j in range(N):
+            tmp2.append(
+                xr.DataArray(
+                    data=eofs[i][j], coords=coords, dims=["lat", "lon"], name=dataname
+                )
+            )
+        tmp.append(xr.concat(tmp2, pd.Index(np.arange(N), name="mode")))
+    eofs = xr.Dataset({datanames[0]: tmp[0], datanames[1]: tmp[1]})
+
+    return eofs, pcs, variance_fraction
+
+
 base_path = "/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/"
 Solar = Generation_type(
     "solar",
-    ["default_panel", "both_constant", "tilt_constant", "neither_constant"],
+    ["both_constant", "tilt_constant", "neither_constant"],
     "output/solar_power/",
     "plots/analysis/hotspots/",
     base_path,
-)  # todo update with scenario names once three solar options ready
+)
 Wind = Generation_type(
     "wind",
     ["E-126_7580", "SWT120_3600", "SWT142_3150"],
@@ -56,120 +89,217 @@ Wind = Generation_type(
     "plots/analysis/hotspots/",
     base_path,
 )
+individual = False
+joint = True
 
-for Generation in [Solar, Wind]:
-    for time_scale_name in ["annual", "ten", "twenty"]:
-        for scenario in Generation.scenarios:
-            Generation.reset_plot_path(
-                "plots/analysis/eof/"
-                + time_scale_name
-                + "/"
-                + Generation.name
-                + "_power/"
-                + scenario
-                + "/"
-            )
-            # load data
-            all_power = Generation.open_data(scenario).load()
-            all_power = running(all_power, time_scale_name)
-            # evaluate data
-            ens_members = list(all_power.number.values)
-            ens_members.append("ensemble_mean")
-            for number in ens_members:
-                # choose ensemble member
-                if number == "ensemble_mean":
-                    power = all_power.mean("number")
-                else:
-                    power = all_power.sel({"number": number})
-                # EOF analysis
-                N_EOFs = 4
-                eofs, pcs, variance_fraction = perform_EOF(
-                    power[Generation.var], N_EOFs
+if individual:
+    for Generation in [Solar, Wind]:  # zip?
+        for time_scale_name in ["annual", "ten", "twenty"]:
+            for scenario in Generation.scenarios:
+                Generation.reset_plot_path(
+                    "plots/analysis/eof/"
+                    + time_scale_name
+                    + "/"
+                    + Generation.name
+                    + "_power/"
+                    + scenario
+                    + "/"
                 )
-                # prepare plots
-                cmap = mpl.cm.get_cmap("RdBu_r")
-                plt.close("all")
-                f, ax = plt.subplots(
-                    ncols=N_EOFs,
-                    nrows=2,
-                    subplot_kw={"projection": ccrs.PlateCarree()},
-                    gridspec_kw={"height_ratios": [3, 1]},
-                    figsize=(12, 6),
-                )
-                plt.subplots_adjust(
-                    left=0.05, right=0.97, bottom=0.15, hspace=0.5, top=0.92
-                )
-                cbar_ax = f.add_axes([0.2, 0.08, 0.6, 0.02])
-                # make plots
-                for i in range(N_EOFs):
-                    vmax = np.round(
-                        np.max([eofs.values.max(), -eofs.values.min()]) * 1.1, 2
-                    )
-                    if i != 0:
-                        plot_field(
-                            eofs.sel({"mode": i}, drop=True),
-                            ax=ax[0, i],
-                            title=str(
-                                np.round(
-                                    variance_fraction.sel({"mode": i}).values * 100, 1
-                                )
-                            )
-                            + "% variance ",
-                            vmin=-vmax,
-                            vmax=vmax,
-                            add_colorbar=False,
-                            cmap=cmap,
-                        )
+                # load data
+                all_power = Generation.open_data(scenario).load()
+                all_power = running(all_power, time_scale_name)
+                # evaluate data
+                ens_members = list(all_power.number.values)
+                ens_members.append("ensemble_mean")
+                for number in ens_members:
+                    # choose ensemble member
+                    if number == "ensemble_mean":
+                        power = all_power.mean("number")
                     else:
-                        plot_field(
-                            eofs.sel({"mode": i}, drop=True),
-                            ax=ax[0, i],
-                            title=str(
-                                np.round(
-                                    variance_fraction.sel({"mode": i}).values * 100, 1
-                                )
-                            )
-                            + "% variance ",
-                            vmin=-vmax,
-                            vmax=vmax,
-                            add_colorbar=True,
-                            cbar_ax=cbar_ax,
-                            cmap=cmap,
-                            cbar_kwargs={
-                                "orientation": "horizontal",
-                                "label": Generation.name + " power generation [a.u.]",
-                            },
+                        power = all_power.sel({"number": number})
+                    # EOF analysis
+                    N_EOFs = 4
+                    eofs, pcs, variance_fraction = perform_EOF(
+                        power[Generation.var], N_EOFs
+                    )
+                    # prepare plots
+                    cmap = mpl.cm.get_cmap("RdBu_r")
+                    plt.close("all")
+                    f, ax = plt.subplots(
+                        ncols=N_EOFs,
+                        nrows=2,
+                        subplot_kw={"projection": ccrs.PlateCarree()},
+                        gridspec_kw={"height_ratios": [3, 1]},
+                        figsize=(12, 6),
+                    )
+                    plt.subplots_adjust(
+                        left=0.05, right=0.97, bottom=0.15, hspace=0.5, top=0.92
+                    )
+                    cbar_ax = f.add_axes([0.2, 0.08, 0.6, 0.02])
+                    # make plots
+                    for i in range(N_EOFs):
+                        vmax = np.round(
+                            np.max([eofs.values.max(), -eofs.values.min()]) * 1.1, 2
                         )
-                    ax[1, i] = plt.subplot(
-                        2, N_EOFs, N_EOFs + 1 + i
-                    )  # override the GeoAxes object
-                    pc = pcs.sel({"mode": i}, drop=True)
-                    pc = pd.Series(data=pc.values, index=pc.time.values)
-                    pc.plot(ax=ax[1, i])
-                    pc.rolling(window=5, center=True).mean().plot(
-                        ax=ax[1, i], ls="--", color="black", lw=2
-                    )
-                plt.suptitle(scenario)
-                if number == "ensemble_mean":
-                    plotname = Generation.name + "power_eofs_mean.png"
-                else:
-                    plotname = (
-                        Generation.name
-                        + "power_eofs_"
-                        + str(int(number))
-                        + ".png"
-                    )
-                add_letters(ax)
-                plt.savefig(Generation.plot_path + plotname)
+                        if i != 0:
+                            plot_field(
+                                eofs.sel({"mode": i}, drop=True),
+                                ax=ax[0, i],
+                                title=str(
+                                    np.round(
+                                        variance_fraction.sel({"mode": i}).values * 100, 1
+                                    )
+                                )
+                                + "% variance ",
+                                vmin=-vmax,
+                                vmax=vmax,
+                                add_colorbar=False,
+                                cmap=cmap,
+                            )
+                        else:
+                            plot_field(
+                                eofs.sel({"mode": i}, drop=True),
+                                ax=ax[0, i],
+                                title=str(
+                                    np.round(
+                                        variance_fraction.sel({"mode": i}).values * 100, 1
+                                    )
+                                )
+                                + "% variance ",
+                                vmin=-vmax,
+                                vmax=vmax,
+                                add_colorbar=True,
+                                cbar_ax=cbar_ax,
+                                cmap=cmap,
+                                cbar_kwargs={
+                                    "orientation": "horizontal",
+                                    "label": Generation.name + " power generation [a.u.]",
+                                },
+                            )
+                        ax[1, i] = plt.subplot(
+                            2, N_EOFs, N_EOFs + 1 + i
+                        )  # override the GeoAxes object
+                        pc = pcs.sel({"mode": i}, drop=True)
+                        pc = pd.Series(data=pc.values, index=pc.time.values)
+                        pc.plot(ax=ax[1, i])
+                        pc.rolling(window=5, center=True).mean().plot(
+                            ax=ax[1, i], ls="--", color="black", lw=2
+                        )
+                    plt.suptitle(scenario)
+                    if number == "ensemble_mean":
+                        plotname = Generation.name + "power_eofs_mean.png"
+                    else:
+                        plotname = (
+                            Generation.name + "power_eofs_" + str(int(number)) + ".png"
+                        )
+                    add_letters(ax)
+                    plt.savefig(Generation.plot_path + plotname)
 
-            # add mean timeseries
-            f, ax = plt.subplots()
-            try:
-                all_power[Generation.var].mean(dim=["latitude", "longitude", "number"]).plot(ax=ax)
-            except ValueError:
-                all_power[Generation.var].mean(dim=["lat", "lon", "number"]).plot(ax=ax)
-            ax.set_title(
-                scenario + " " + Generation.name + " generation (mean over Europe, " + time_scale_name + " y, ensemble)"
+                # add mean timeseries
+                f, ax = plt.subplots()
+                try:
+                    all_power[Generation.var].mean(
+                        dim=["latitude", "longitude", "number"]
+                    ).plot(ax=ax)
+                except ValueError:
+                    all_power[Generation.var].mean(dim=["lat", "lon", "number"]).plot(ax=ax)
+                ax.set_title(
+                    scenario
+                    + " "
+                    + Generation.name
+                    + " generation (mean over Europe, "
+                    + time_scale_name
+                    + " y, ensemble)"
+                )
+                plt.tight_layout()
+                plt.savefig(Generation.plot_path + "/" + scenario + "mean_timeseries.png")
+
+
+"""
+multivariate EOF
+
+multi-variate EOF has one set of PCs and different EOFs for each input field
+"""
+if joint:
+    # load solar (almost no variability in solar, so stick to one member)
+    solar_scen = "both_constant"
+    solar_power = Solar.open_data(solar_scen).mean("number").load()
+    solar_power = running(solar_power, "twenty")
+    solar_power["time"] += pd.Timedelta(90, unit="m")  # Solar power is 1:30h shifted back
+    # normalize with long-term mean
+    solar_power /= solar_power.mean("time")
+    for wind_scen in Wind.scenarios:
+        # load wind
+        wind_power = Wind.open_data(wind_scen).mean("number").load()
+        wind_power = running(wind_power, "twenty")
+        wind_power /= wind_power.mean("time")
+        # MEOF analysis
+        N_EOFs = 4
+        data = [
+            data.values for data in xr.align(solar_power[Solar.var], wind_power[Wind.var])
+        ]
+        eofs, pcs, variance_fraction = perform_EOF_multi(
+            data, N_EOFs, solar_power.drop("time").coords, [Solar.var, Wind.var]
+        )
+        # prepare plots
+        cmap = mpl.cm.get_cmap("RdBu_r")
+        plt.close("all")
+        f = plt.figure(figsize=(12, 10))
+        ax = [plt.subplot(3, N_EOFs, i, projection= ccrs.PlateCarree()) for i in range(1,9)]
+        ax.extend([plt.subplot(3, N_EOFs, i) for i in range(9, 13)])
+        ax = np.asarray(ax).reshape(3, 4)
+        plt.subplots_adjust(left=0.05, right=0.97, bottom=0.15, hspace=0.5, top=0.92)
+        cbar_ax = f.add_axes([0.2, 0.08, 0.6, 0.02])
+        # make plots
+        vmax = .08
+
+        for i in range(N_EOFs):
+            if i != 0:
+                plot_field(
+                    eofs[Solar.var].sel({"mode": i}, drop=True),
+                    ax=ax[0, i],
+                    title=str(np.round(variance_fraction[i] * 100, 1)) + "% joint variance ",
+                    vmin=-vmax,
+                    vmax=vmax,
+                    extend='both',
+                    add_colorbar=False,
+                    cmap=cmap,
+                )
+            else:
+                plot_field(
+                    eofs[Solar.var].sel({"mode": i}, drop=True),
+                    ax=ax[0, i],
+                    title=str(np.round(variance_fraction[i] * 100, 1)) + "% joint variance ",
+                    vmin=-vmax,
+                    vmax=vmax,
+                    add_colorbar=True,
+                    cbar_ax=cbar_ax,
+                    cmap=cmap,
+                    extend='both',
+                    cbar_kwargs={
+                        "orientation": "horizontal",
+                        "label": r"Relative wind or solar power generation $\frac{CF_{20}}{CF_{LT}}$ [a.u.]",
+                    },
+                )
+            plot_field(
+                eofs[Wind.var].sel({"mode": i}, drop=True),
+                ax=ax[1, i],
+                vmin=-vmax,
+                vmax=vmax,
+                add_colorbar=False,
+                cmap=cmap,
+                extend='both',
             )
-            plt.tight_layout()
-            plt.savefig(Generation.plot_path + "/" + scenario + "mean_timeseries.png")
+            pc = pcs[:, i]
+            pc = pd.Series(data=pc, index=wind_power.time.values)
+            pc.plot(ax=ax[2, i])
+            pc.rolling(window=5, center=True).mean().plot(
+                ax=ax[2, i], ls="--", color="black", lw=2
+            )
+            add_row_label(ax[0, 0], "Solar")
+            add_row_label(ax[1, 0], "Wind")
+
+        plt.suptitle(wind_scen)
+        plotname = wind_scen + "_power_meofs_mean.png"
+        add_letters(ax)
+        plt.savefig("/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/plots/analysis/eof/twenty/"+ plotname)
