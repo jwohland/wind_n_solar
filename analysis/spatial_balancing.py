cd048a14391190a33b00f518362b3965e5a75eab
jwohland <jwohland@ethz.ch>
2020-10-07 10:36:38 +0200

    Formatting
    Added new scenarios (wind-only, solar-only)
    
    Added weighted mean of individual countries

--- analysis/spatial_balancing.py
+++ analysis/spatial_balancing.py
@@ -23,9 +23,8 @@ def plot_histograms(wind_shares, solar_shares, plotname):
     with open(base_path + "output/country_generation.pickle", "rb") as handle:
         country_generation = pickle.load(handle)
 
-    amplitude_dict = {}
+    amplitude_dict, results = {}, {}
 
-    results = {}
     for data_timescale in ["seasonal", "multidecadal"]:
         for i, country_group in enumerate(country_generation[data_timescale].keys()):
             G_c = country_generation[data_timescale][country_group]
@@ -39,7 +38,9 @@ def plot_histograms(wind_shares, solar_shares, plotname):
             float((G_tot.max() - G_tot.min()) / G_tot.min() * 100), 1
         )
         for j, exclude in enumerate(country_generation[data_timescale].keys()):
-            for i, country_group in enumerate(country_generation[data_timescale].keys()):
+            for i, country_group in enumerate(
+                country_generation[data_timescale].keys()
+            ):
                 if j == 0:
                     amplitude_dict[country_group] = {}
                 G_c = country_generation[data_timescale][country_group]
@@ -48,17 +49,21 @@ def plot_histograms(wind_shares, solar_shares, plotname):
                         solar_shares[i] * G_c["solar"] + wind_shares[i] * G_c["wind"]
                     )
                     amplitude_dict[exclude]["alone"] = np.round(
-                        float((G_exclude.max() - G_exclude.min()) / G_exclude.min() * 100),
+                        float(
+                            (G_exclude.max() - G_exclude.min()) / G_exclude.min() * 100
+                        ),
                         1,
                     )
                 else:
                     if (i == 0) or (i == 1 and j == 0):
                         G_tot = (
-                            solar_shares[i] * G_c["solar"] + wind_shares[i] * G_c["wind"]
+                            solar_shares[i] * G_c["solar"]
+                            + wind_shares[i] * G_c["wind"]
                         )
                     else:
                         G_tot += (
-                            solar_shares[i] * G_c["solar"] + wind_shares[i] * G_c["wind"]
+                            solar_shares[i] * G_c["solar"]
+                            + wind_shares[i] * G_c["wind"]
                         )
                     amplitude_dict[exclude]["others"] = np.round(
                         float((G_tot.max() - G_tot.min()) / G_tot.min() * 100), 1
@@ -67,29 +72,33 @@ def plot_histograms(wind_shares, solar_shares, plotname):
             data=amplitude_dict, orient="columns"
         )
 
-
     # plotting
-    fs=12
+    fs = 12
     f, axs = plt.subplots(ncols=2, figsize=(12, 8))
     for i, data_timescale in enumerate(results.keys()):
         ax = axs[i]
         ax.set_xticks(range(9))
-        ticklabels = list(results[data_timescale].keys()[1:-1])  # for plotting asthaetics, last group with linebreak
-        ticklabels.append("Greece, Bulgaria, Serbia, Croatia \n Bosnia and Herzogovina, Romania, Albania")
+        ticklabels = list(
+            results[data_timescale].keys()[1:-1]
+        )  # for plotting asthaetics, last group with linebreak
+        ticklabels.append(
+            "Greece, Bulgaria, Serbia, Croatia \n Bosnia and Herzogovina, Romania, Albania"
+        )
         ax.set_xticklabels(
-            ticklabels,
-            rotation=45,
-            ha="right",
-            va="center",
-            rotation_mode="anchor",
+            ticklabels, rotation=45, ha="right", va="center", rotation_mode="anchor",
         )
         ax.axhline(
-            results[data_timescale]["All"]["others"], color="red", ls="--", label="Europe"
+            results[data_timescale]["All"]["others"],
+            color="red",
+            ls="--",
+            label="Cooperation",
         )
         ax.set_ylabel(
-            r"Amplitude of variability   $\frac{G_\mathrm{max} -G_\mathrm{min}}{G_\mathrm{min}}$ [%]", fontsize=fs
+            r"Amplitude of variability   $\frac{G_\mathrm{max} -G_\mathrm{min}}{G_\mathrm{min}}$ [%]",
+            fontsize=fs,
         )
-        ax.set_title(data_timescale, fontsize=fs+1)
+        ax.set_title(data_timescale, fontsize=fs + 1)
+        weighted_mean_amp = 0
         for x_off, countries in enumerate(results[data_timescale].keys()[1:]):
             w = 0.3
             ax.bar(
@@ -107,6 +116,12 @@ def plot_histograms(wind_shares, solar_shares, plotname):
                 color="blue",
                 label="Others" if (i == 1 and x_off == 0) else "",
             )
+            weighted_mean_amp += (
+                (solar_shares[x_off] + wind_shares[x_off])
+                / 100
+                * results[data_timescale][countries]["alone"]
+            )
+        ax.axhline(weighted_mean_amp, color="grey", ls="--", label="Mean Isolated")
     plt.legend(fontsize=fs)
     add_letters(axs)
     plt.subplots_adjust(bottom=0.3, left=0.075, top=0.95, right=0.98)
@@ -114,8 +129,9 @@ def plot_histograms(wind_shares, solar_shares, plotname):
 
 
 """
-SCENARIO1: current installed capacities
+Different scenarios of installed capacities
 """
+# current shares (sum(solar_shares) + sum(wind_shares)=1)
 wind_shares = [
     8.6,
     9.4,
@@ -126,7 +142,7 @@ wind_shares = [
     2.0,
     1.0,
     2.5,
-]  #  Country wind capacity divided by European wind + solar capacity
+]
 solar_shares = [
     4.1,
     3.6,
@@ -137,9 +153,16 @@ solar_shares = [
     1.6,
     0.1,
     1.6,
-]  #  Country solar capacity divided by European wind + solar capacity
+]
+
 plot_histograms(wind_shares, solar_shares, "current")
+plot_histograms(
+    [x / np.sum(wind_shares) * 100 for x in wind_shares], [0] * 9, "current_windonly"
+)
+plot_histograms(
+    [0] * 9, [x / np.sum(solar_shares) * 100 for x in solar_shares], "current_solaronly"
+)
 
-plot_histograms([np.mean(wind_shares)]*9, solar_shares, "even_wind")
-
-plot_histograms([np.mean(wind_shares)]*9, [np.mean(solar_shares)]*9, "even_both")
+#even distribution of wind or solar while keeping total solar to total wind ratio untouched
+plot_histograms([np.mean(wind_shares)] * 9, solar_shares, "even_wind")
+plot_histograms([np.mean(wind_shares)] * 9, [np.mean(solar_shares)] * 9, "even_both")
