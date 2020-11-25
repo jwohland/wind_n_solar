1a834b90af0c968b57f09d229ea34e1f00f28b16
Stefan Pfenninger <stefan@pfenninger.org>
2020-11-25 10:28:36 +0100

    New version of balancing_overview figure

---
+++ analysis/balancing_overview.py
@@ -0,0 +1,189 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+import spatial_balancing
+
+
+sns.set_context("notebook", font_scale=1.2)
+sns.set_style("ticks", {"axes.linewidth": 1.0, "font.sans-serif": "Helvetica"})
+plt.rcParams.update(
+    {
+        # Use sans math font
+        "text.usetex": False,
+        "mathtext.fontset": "stixsans",
+        "svg.fonttype": "none",  # Make text editable
+    }
+)
+
+
+def get_and_clean_data():
+    scenario_results, _, _, = spatial_balancing.load_scenario_results()
+
+    # Subplot A
+
+    SHORT_NAMES = {
+        "United Kingdom, Ireland": "UK, IR",
+        "Portugal, Spain": "PT, ES",
+        "France, Belgium, Netherlands": "FR, BE, NL",
+        "Germany, Denmark": "DE, DK",
+        "Italy, Austria, Switzerland, Slovenia": "IT, AU, CH, SI",
+        "Sweden, Norway": "SE, NO",
+        "Poland, Czech Republic, Slovakia, Hungary": "PL, CZ, SK, HU",
+        "Lithuania, Latvia, Estonia, Finland": "LT, LV, EE, FI",
+        "Greece, Bulgaria, Serbia, Croatia, Bosnia and Herzogovina, Romania, Albania": "GR, BG, RS, HR, BA, RO, AL",
+    }
+
+    for k, v in SHORT_NAMES.items():
+        SHORT_NAMES[k] = v.replace(", ", "\n")
+
+    df_countries = scenario_results["Both"]["multidecadal"].T
+
+    mean_coop = df_countries.loc["All", "others"]
+    mean_isolated = df_countries.loc["Mean_isolated", "others"]
+
+    df_countries = df_countries.rename(
+        columns={"others": "Others", "alone": "Isolated"}, index=SHORT_NAMES
+    )
+
+    df_countries = df_countries.drop(["All", "Mean_isolated"], axis=0)
+
+    # Subplot B
+
+    df_europe = pd.DataFrame(
+        {k: scenario_results[k]["multidecadal"]["All"] for k in scenario_results.keys()}
+    ).T
+    df_europe_isolated = pd.DataFrame(
+        {
+            k: scenario_results[k]["multidecadal"]["Mean_isolated"]
+            for k in scenario_results.keys()
+        }
+    ).T
+    df_europe["Isolation penalty"] = df_europe_isolated["others"] - df_europe["others"]
+    df_europe = df_europe.rename(columns={"others": "Cooperation"}).drop(
+        "alone", axis=1
+    )
+
+    return df_countries, df_europe, mean_coop, mean_isolated
+
+
+def draw_plot(df_countries, df_europe, mean_coop, mean_isolated):
+    COLORS = {
+        "others": "#5489bd",
+        "isolated": "#fdc272",
+        "cooperation": "#af4d80",
+    }
+
+    fig, (ax0, ax1) = plt.subplots(nrows=1, ncols=2, figsize=(12, 6))
+
+    # SUBPLOT A
+
+    ax = ax0
+
+    df_countries.plot.bar(
+        ax=ax,
+        rot=0,
+        width=0.7,
+        legend=False,
+        color=[COLORS["others"], COLORS["isolated"]],
+    )
+
+    ax.set_ylabel("% Amplitude of variability")
+
+    axhline_kwargs = dict(xmax=1.2, clip_on=False)
+    ax.axhline(
+        mean_coop,
+        color=COLORS["cooperation"],
+        label="Mean with cooperation",
+        **axhline_kwargs
+    )
+    ax.axhline(
+        mean_isolated,
+        color=COLORS["isolated"],
+        label="Mean with isolation",
+        **axhline_kwargs
+    )
+
+    # Arrow
+    ax.arrow(
+        x=9.5,
+        y=mean_isolated,
+        dx=0,
+        dy=-1 * (mean_isolated - mean_coop),
+        color=COLORS["cooperation"],
+        length_includes_head=True,
+        width=0.2,
+        head_width=0.5,
+        head_length=0.35,
+        clip_on=False,
+    )
+
+    handles, labels = ax.get_legend_handles_labels()
+    handles = [handles[-2], handles[-1], handles[1], handles[0]]
+    labels = [labels[-2], labels[-1], labels[1], labels[0]]
+    ax.legend(handles, labels, frameon=False, ncol=2)
+
+    ax.set_title(r"$\bf{a.}$ Country groups for current capacities", loc="left")
+
+    # SUBPLOT B
+
+    ax = ax1
+
+    df_europe.plot.bar(
+        ax=ax,
+        stacked=True,
+        rot=0,
+        legend=False,
+        color=[COLORS["cooperation"], COLORS["isolated"]],
+    )
+
+    axhline_kwargs = dict(xmax=0.09)
+    ax.axhline(mean_coop, color=COLORS["cooperation"], **axhline_kwargs)
+    ax.axhline(mean_isolated, color=COLORS["isolated"], **axhline_kwargs)
+
+    axvline_kwargs = dict(ymin=-0.315, ymax=1.0, clip_on=False, color="grey", alpha=0.2)
+    for x in [-0.5, 2.5, 5.5, 7.5]:
+        ax.axvline(x, **axvline_kwargs)
+    ax.axhline(-2.2, clip_on=False, color="grey", alpha=0.2)
+
+    arrow_marker_kwargs = dict(color=COLORS["cooperation"], alpha=0.4)
+    ax.vlines(
+        -1.325, ymin=mean_isolated, ymax=6.45, clip_on=False, **arrow_marker_kwargs
+    )
+    ax.hlines(6.45, xmin=-1.325, xmax=0.3, clip_on=False, **arrow_marker_kwargs)
+    ax.text(
+        0.4,
+        6.45,
+        "Benefit of\ncooperation",
+        horizontalalignment="left",
+        verticalalignment="center",
+        fontsize=13,
+        bbox=dict(facecolor="none", edgecolor=COLORS["cooperation"], alpha=0.4),
+    )
+
+    text_kwargs = dict(
+        horizontalalignment="center", verticalalignment="top", fontsize=11
+    )
+    ax.text(1, -1, "Current capacity\nand distribution", **text_kwargs)
+    ax.text(4, -1, "Current capacity\nand even\nplant distribution", **text_kwargs)
+    ax.text(
+        6.5, -1, "Current plant\ndistribution\nand adjusted\nwind share", **text_kwargs
+    )
+
+    handles, labels = ax.get_legend_handles_labels()
+    ax.legend(handles[::-1], labels[::-1], frameon=False)
+
+    ax.set_title(r"$\bf{b.}$ Europe-wide mean for all scenarios", loc="left")
+
+    # CLEAN UP PLOTS
+
+    for ax in [ax0, ax1]:
+        ax.set_ylim(0, 7)
+
+    return fig
+
+
+if __name__ == "__main__":
+    df_countries, df_europe, mean_coop, mean_isolated = get_and_clean_data()
+    fig = draw_plot(df_countries, df_europe, mean_coop, mean_isolated)
+    plt.savefig("balancing_overview.png", bbox_inches="tight", pad_inches=0.1, dpi=300)
--- analysis/spatial_balancing.py
+++ analysis/spatial_balancing.py
@@ -13,7 +13,9 @@ from utils import add_letters
 mpl.rcParams["axes.spines.left"] = True
 mpl.rcParams["axes.spines.bottom"] = True
 
-BASE_PATH = "/cluster/work/apatt/wojan/renewable_generation/wind_n_solar/"
+import os
+
+BASE_PATH = os.path.join(os.path.dirname(__file__), "../")
 
 
 def calc_histograms(wind_shares, solar_shares):
@@ -75,11 +77,11 @@ def calc_histograms(wind_shares, solar_shares):
     return results
 
 
-def plot_histograms(results, plotname):
+def plot_histograms(results, plotname, solar_shares, wind_shares):
     # plotting
     fs = 12
     f, axs = plt.subplots(ncols=2, figsize=(12, 8))
-    plot_path = BASE_PATH + "plots/analysis/country_assessment/"
+    plot_path = BASE_PATH + "plots/"
     for i, data_timescale in enumerate(results.keys()):
         ax = axs[i]
         ax.set_xticks(range(9))
@@ -99,8 +101,7 @@ def plot_histograms(results, plotname):
             label="Cooperation",
         )
         ax.set_ylabel(
-            r"Amplitude of variability   $\frac{G_\mathrm{max} -G_\mathrm{min}}{G_\mathrm{min}}$ [%]",
-            fontsize=fs,
+            "% Amplitude of variability", fontsize=fs,
         )
         ax.set_title(data_timescale, fontsize=fs + 1)
         weighted_mean_amp = 0
@@ -130,40 +131,45 @@ def plot_histograms(results, plotname):
     plt.legend(fontsize=fs)
     add_letters(axs)
     plt.subplots_adjust(bottom=0.3, left=0.075, top=0.95, right=0.98)
-    plt.savefig(plot_path + "balancing_" + plotname + ".jpeg", dpi=300)
+    # plt.savefig(plot_path + "balancing_" + plotname + ".jpeg", dpi=300)
     results["multidecadal"]["Mean_isolated"] = weighted_mean_amp
     return results
 
 
-def plot_overview_hists(scenario_results):
+COLORS = {
+    "isolated": "chocolate",
+    "cooperation": "firebrick",
+}
+
+
+def plot_overview_hists(scenario_results, solar_shares, wind_shares):
     fs = 12
     f = plt.figure(figsize=(12, 8))
-    axs = np.array([plt.subplot(121), SubplotHost(f, 122)])  # SubplotHost instance needed for double x axis
+    axs = np.array(
+        [plt.subplot(121), SubplotHost(f, 122)]
+    )  # SubplotHost instance needed for double x axis
     for ax in axs:
         f.add_subplot(ax)
-    plot_path = BASE_PATH + "plots/analysis/country_assessment/"
+    plot_path = BASE_PATH + "plots/"
     # plot hist of currently installed capacities
-    results = scenario_results["wind + solar"]
+    results = scenario_results["Both"]
     data_timescale = "multidecadal"
     axs[0].set_xticks(range(9))
     ticklabels = list(
         results[data_timescale].keys()[1:-2]
     )  # for plotting asthaetics, last group with linebreak
-    ticklabels.append(
-        "Greece, Bulgaria, Serbia, Croatia \n Bosnia and Herzogovina, Romania, Albania"
-    )
+    ticklabels.append("GR, BG, RS, HR, BA, RO, AL")
     axs[0].set_xticklabels(
         ticklabels, rotation=45, ha="right", va="center", rotation_mode="anchor",
     )
     axs[0].axhline(
         results[data_timescale]["All"]["others"],
-        color="firebrick",
+        color=COLORS["cooperation"],
         ls="--",
         label="Cooperation",
     )
     axs[0].set_ylabel(
-        r"Amplitude of variability   $\frac{G_\mathrm{max} -G_\mathrm{min}}{G_\mathrm{min}}$ [%]",
-        fontsize=fs,
+        "% Amplitude of variability", fontsize=fs,
     )
     weighted_mean_amp = 0
     for x_off, countries in enumerate(results[data_timescale].keys()[1:-1]):
@@ -172,8 +178,8 @@ def plot_overview_hists(scenario_results):
             x_off - w / 2,
             results[data_timescale][countries]["alone"],
             w,
-            color="grey",
-            alpha=0.6,
+            color=COLORS["isolated"],
+            alpha=0.8,
             label="Isolated" if x_off == 0 else "",
         )
         axs[0].bar(
@@ -193,10 +199,10 @@ def plot_overview_hists(scenario_results):
     )
     axs[0].arrow(
         x=x_off + 0.55,
-        y=results[data_timescale]["All"]["others"],
+        y=weighted_mean_amp,
         dx=0,
-        dy=weighted_mean_amp - results[data_timescale]["All"]["others"],
-        color="chocolate",
+        dy=-1 * (weighted_mean_amp - results[data_timescale]["All"]["others"]),
+        color=COLORS["cooperation"],
         length_includes_head=True,
         width=0.2,
         head_width=0.5,
@@ -204,12 +210,11 @@ def plot_overview_hists(scenario_results):
         head_length=0.35,
     )
     axs[0].set_xlim(xmax=8.85)
-    axs[0].legend(fontsize=fs, ncol=2)
+    axs[0].legend(fontsize=fs, ncol=2, frameon=False)
 
     # overview histogram with cooperation and isolation penalty
     axs[1].set_ylabel(
-        r"Amplitude of variability   $\frac{G_\mathrm{max} -G_\mathrm{min}}{G_\mathrm{min}}$ [%]",
-        fontsize=fs,
+        "% Amplitude of variability", fontsize=fs,
     )
     for x_off, scenario in enumerate(scenario_results.keys()):
         results = scenario_results[scenario]["multidecadal"]
@@ -224,123 +229,163 @@ def plot_overview_hists(scenario_results):
             x_off,
             results["Mean_isolated"] - results["All"],
             bottom=results["All"],
-            color="chocolate",
+            color=COLORS["isolated"],
             alpha=0.8,
             label="Isolation penalty" if x_off == 0 else "",
         )
 
-    axs[1].legend(fontsize=fs)
+    axs[1].legend(fontsize=fs, frameon=False)
     axs[1].set_xticks(np.arange(8))
     axs[1].set_xticklabels([x for x in scenario_results.keys()],)
     plt.setp(
         axs[1].axis["bottom"].major_ticklabels,
-        rotation=45,
-        ha="right",
-        va="center",
-        rotation_mode="anchor",
+        #     rotation=45,
+        #     ha="center",
+        va="top",
+        #     rotation_mode="anchor",
     )
 
     # Add a second line of xlabels
     ax2 = axs[1].twiny()
-    offset = -47, -65  # Position of the second axis
+    offset = 0, -30  # Position of the second axis
     new_axisline = ax2.get_grid_helper().new_fixed_axis
     ax2.axis["bottom"] = new_axisline(loc="bottom", axes=ax2, offset=offset)
     ax2.axis["top"].set_visible(False)
-    axs[1].axis["right"].set_visible(False) # axis cloning activates right hand side y axis
+    axs[1].axis["right"].set_visible(
+        False
+    )  # axis cloning activates right hand side y axis
 
-    ax2.set_xticks([-.5, 2.5, 5.2, 7.5])
+    ax2.set_xticks([-0.5, 2.5, 5.2, 7.5])
     ax2.xaxis.set_major_formatter(ticker.NullFormatter())
     ax2.xaxis.set_minor_locator(ticker.FixedLocator([1, 3.8, 6.4]))
     ax2.axis["bottom"].minor_ticks.set_ticksize(0)
     ax2.xaxis.set_minor_formatter(
         ticker.FixedFormatter(
-            ["current", "same total", "same distribution"]
+            [
+                "Current capacity\nand distribution",
+                "Current capacity\nwith even\ndistribution of plants",
+                "Current distribution\nof plants with\nadjusted share of wind",
+            ]
         )
     )
     # Add lines for orientation between both x axes
     def add_line(f, x1):
-        x1 /= 3600.
-        x0 = x1 + 220./3600
-        y0 = 1-1705./2400
-        y1 = 1-1925./2400
-        line = plt.Line2D([x0, x1], [y0, y1], color="grey", linestyle='--', alpha=.4, transform=f.transFigure)
+        x1 /= 3600.0
+        x0 = x1
+        # x0 = x1 + 220.0 / 3600
+        y0 = 1 - 1705.0 / 2400
+        y1 = 1 - 1925.0 / 2400
+        line = plt.Line2D(
+            [x0, x1],
+            [y0, y1],
+            color="grey",
+            linestyle="--",
+            alpha=0.4,
+            transform=f.transFigure,
+        )
         f.add_artist(line)
 
-    for x1 in [2410, 2910]:
+    for x1 in [2610, 3130]:
         add_line(f, x1)
 
     add_letters(axs, fs=12)
     for i in range(2):
         axs[i].set_ylim(ymax=6.8)
     plt.subplots_adjust(bottom=0.3, left=0.075, top=0.95, right=0.98)
-    plt.savefig(plot_path + "balancing_overview.jpeg", dpi=300)
+    # plt.savefig(plot_path + "balancing_overview.jpeg", dpi=300)
+
+    return f, axs
 
 
-"""
-Different scenarios of installed capacities
-"""
-# current shares (sum(solar_shares) + sum(wind_shares)=1)
-wind_shares = [
-    8.6,
-    9.4,
-    7.5,
-    20.4,
-    4.3,
-    3.5,
-    2.0,
-    1.0,
-    2.5,
-]
-solar_shares = [
-    4.1,
-    3.6,
-    6.7,
-    15.3,
-    7.7,
-    0.2,
-    1.6,
-    0.1,
-    1.6,
-]
+def load_scenario_results():
+    """
+    Different scenarios of installed capacities
+    """
+    # current shares (sum(solar_shares) + sum(wind_shares)=1)
+    wind_shares = [
+        8.6,
+        9.4,
+        7.5,
+        20.4,
+        4.3,
+        3.5,
+        2.0,
+        1.0,
+        2.5,
+    ]
+    solar_shares = [
+        4.1,
+        3.6,
+        6.7,
+        15.3,
+        7.7,
+        0.2,
+        1.6,
+        0.1,
+        1.6,
+    ]
 
-scenario_results = {}
+    scenario_results = {}
 
-scenario_results["wind + solar"] = plot_histograms(
-    calc_histograms(wind_shares, solar_shares), "current"
-)
-scenario_results["wind"] = plot_histograms(
-    calc_histograms([x / np.sum(wind_shares) * 100 for x in wind_shares], [0] * 9),
-    "current_windonly",
-)
-scenario_results["solar"] = plot_histograms(
-    calc_histograms([0] * 9, [x / np.sum(solar_shares) * 100 for x in solar_shares]),
-    "current_solaronly",
-)
+    scenario_results["Both"] = plot_histograms(
+        calc_histograms(wind_shares, solar_shares), "current", solar_shares, wind_shares
+    )
+    scenario_results["Wind"] = plot_histograms(
+        calc_histograms([x / np.sum(wind_shares) * 100 for x in wind_shares], [0] * 9),
+        "current_windonly",
+        solar_shares,
+        wind_shares,
+    )
+    scenario_results["Solar"] = plot_histograms(
+        calc_histograms(
+            [0] * 9, [x / np.sum(solar_shares) * 100 for x in solar_shares]
+        ),
+        "current_solaronly",
+        solar_shares,
+        wind_shares,
+    )
 
-# even distribution of wind or solar while keeping total solar to total wind ratio untouched
-scenario_results["even wind"] = plot_histograms(
-    calc_histograms([np.mean(wind_shares)] * 9, solar_shares), "even_wind"
-)
-scenario_results["even solar"] = plot_histograms(
-    calc_histograms(wind_shares, [np.mean(solar_shares)] * 9), "even_solar"
-)
-scenario_results["even both"] = plot_histograms(
-    calc_histograms([np.mean(wind_shares)] * 9, [np.mean(solar_shares)] * 9),
-    "even_both",
-)
+    # even distribution of wind or solar while keeping total solar to total wind ratio untouched
+    scenario_results["Both\neven"] = plot_histograms(
+        calc_histograms([np.mean(wind_shares)] * 9, [np.mean(solar_shares)] * 9),
+        "even_both",
+        solar_shares,
+        wind_shares,
+    )
+    scenario_results["Wind\neven"] = plot_histograms(
+        calc_histograms([np.mean(wind_shares)] * 9, solar_shares),
+        "even_wind",
+        solar_shares,
+        wind_shares,
+    )
+    scenario_results["Solar\neven"] = plot_histograms(
+        calc_histograms(wind_shares, [np.mean(solar_shares)] * 9),
+        "even_solar",
+        solar_shares,
+        wind_shares,
+    )
 
-# keeping inter-country distribution of wind and solar untouched while changing the continental wind/solar share
-def rescale(shares, target_share):
-    return [x / np.sum(shares) * target_share for x in shares]
+    # keeping inter-country distribution of wind and solar untouched while changing the continental wind/solar share
+    def rescale(shares, target_share):
+        return [x / np.sum(shares) * target_share for x in shares]
+
+    scenario_results["40%\nWind"] = plot_histograms(
+        calc_histograms(rescale(wind_shares, 40), rescale(solar_shares, 60)),
+        "windshare_40",
+        solar_shares,
+        wind_shares,
+    )
+    scenario_results["80%\nWind"] = plot_histograms(
+        calc_histograms(rescale(wind_shares, 80), rescale(solar_shares, 20)),
+        "windshare_80",
+        solar_shares,
+        wind_shares,
+    )
+
+    return scenario_results, solar_shares, wind_shares
 
 
-scenario_results["40% wind"] = plot_histograms(
-    calc_histograms(rescale(wind_shares, 40), rescale(solar_shares, 60)), "windshare_40"
-)
-scenario_results["80% wind"] = plot_histograms(
-    calc_histograms(rescale(wind_shares, 80), rescale(solar_shares, 20)), "windshare_80"
-)
-
-
-# make paper overview Figure
-plot_overview_hists(scenario_results)
+if __name__ == "__main__":
+    # make paper overview Figure
+    scenario_results, solar_shares, wind_shares = load_scenario_results()
+    plot_overview_hists(scenario_results, solar_shares, wind_shares)
